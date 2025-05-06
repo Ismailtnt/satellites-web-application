9c039c412550e0210bbe77aaaeecfe47ba8a19f9
Ismail <ismailaitmoumad1@gmail.com>
2025-05-06 12:17:15 +0100

    Fixing subsystem_files View and preventing SQL injection

--- api/myapi/urls.py
+++ api/myapi/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('satellites/', views.satellite_list),
-    path('satellites/1/subsystems/<int:sub_id>/files/', views.subsystem_files),
+    path('satellites/<int:sat_id>/subsystems/<int:sub_id>/files/', views.subsystem_files),
     path('satellites/<int:sat_id>/subsystems/<int:sub_id>/files/<int:file_id>/', views.file_metadata),
     path('satellites/<int:sat_id>/subsystems/<int:sub_id>/files/<int:file_id>/download/', views.download_file),
     #path('satellites/<int:sat_id>fig/', views.SatelliteConfigView.as_view()),
--- api/myapi/views.py
+++ api/myapi/views.py
@@ -8,12 +8,12 @@ from .serializers import *
 def get_satellite(sat_id):
     """
     Returns the name of the satellite. 
-    We have only two satellites, "SAT-MOHAMMEDIA" and "UM5-EOSAT"
+    We have only two satellites, "MOHAMMEDIA-SAT" and "UM5-EOSAT"
     """
     if sat_id == 1:
         return "UM5-EOSAT"
     elif sat_id == 2:
-        return "SAT-MOHAMMEDIA"
+        return "MOHAMMEDIA-SAT"
     else:
         raise ValueError("This satellite do not exist in the database.")    
 
@@ -23,15 +23,24 @@ def satellite_list(request):
     GET /api/satellites 
     Retrieve all satellites
     """
-    data = {"satellites": [{"id":1, "name": "UM5-EOSAT"}, {"id": 2, "name": "SAT-MOHAMMEDIA"}]}
+    data = {"satellites": [{"id":1, "name": "UM5-EOSAT"}, {"id": 2, "name": "MOHAMMEDIA-SAT"}]}
     return Response({"status": "success", "data": data})
     
 
 @api_view(['GET'])
-def subsystem_files(request, sub_id):
-    files = SatFiles.objects.filter(subsystem_id=sub_id)
-    serializer = SatFilesSerializer(files, many=True)
-    return Response(serializer.data)
+def subsystem_files(request, sat_id, sub_id):
+    """
+    GET api/satellites/<int:sat_id>/subsystems/<int:sub_id>/files/
+    Retrieves all files id related to the specific subsystem
+    """
+    name = get_satellite(sat_id)
+    with connection.cursor() as cursor:
+        query = f'''Select id FROM "{name}".sat_files WHERE subsystem_id = {sub_id}'''
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns_name = [col[0] for col in cursor.description] # The description attribute stores metadata about each column
+    data = [dict(zip(columns_name, row)) for row in rows]
+    return Response({"status": "success","data": data})
 
 @api_view(['GET'])
 def file_metadata(request, sat_id, sub_id, file_id):
